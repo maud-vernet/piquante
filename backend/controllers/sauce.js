42106414204f386a64b9ba72fd1cfbd663eec6c4
maud-vernet <vernet.maud@orange.fr>
2023-02-24 11:41:23 +0100

    feat modifySauce + fix images management

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,4 +1,5 @@
 const Sauce = require('../models/sauces');
+const fs = require('fs');
 
 exports.getAllSauces = (req, res, next) => {
     Sauce.find().then(
@@ -43,4 +44,26 @@ exports.createSauce = (req, res, next) => {
   sauce.save()
   .then(() => { res.status(201).json({message: 'Sauce enregistrée !'})})
   .catch(error => { res.status(400).json( { error })})
+};
+
+exports.modifySauce = (req, res, next) => {
+  const sauceObject = req.file ? {
+    ...JSON.parse(req.body.sauce),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body };
+
+  delete sauceObject._userId;
+  Sauce.findOne({_id: req.params.id})
+      .then((sauce) => {
+          if (sauce.userId != req.auth.userId) {
+              res.status(401).json({ message : 'Not authorized'});
+          } else {
+              Sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
+              .then(() => res.status(200).json({message : 'Sauce modifiée !'}))
+              .catch(error => res.status(401).json({ error }));
+          }
+      })
+      .catch((error) => {
+          res.status(400).json({ error });
+      });
 };
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -9,5 +9,6 @@ const sauceCtrl = require('../controllers/sauce');
 router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 
 module.exports = router;
