ccfc304d284d48d82c830e5517af5a396c36ea79
maud-vernet <vernet.maud@orange.fr>
2023-03-01 10:59:33 +0100

    feat add mongo db error handler plugin to user model

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+var mongodbErrorHandler = require('mongoose-mongodb-errors');
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
 });
 
 userSchema.plugin(uniqueValidator);
+userSchema.plugin(mongodbErrorHandler);
 
 module.exports = mongoose.model('User', userSchema);
